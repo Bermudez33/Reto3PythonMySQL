e721d49bd12366af131dd0e46ab66eca47ef3bb4
Bermudez33 <dx.bermudez@gmail.com>
2024-05-03 15:54:35 -0600

    commit ID2

--- connection_database.py
+++ connection_database.py
@@ -1,15 +1,49 @@
 import mysql.connector
+import json
 
-connection = mysql.connector.connect(
+mydb = mysql.connector.connect(
   host="localhost",
   user="root",
   password="",
-  database= "digital_nao_reto_3"
+  database= "sys",
+  port=""
 )
 
-if connection.is_connected():
-    print('Connected Succesfully')
-else:
-    print('Failed to connect')
+mycursor = mydb.cursor()
 
-connection.close()
+with open('C:\\Users\\dxber\\Downloads\\tweets_extraction.json','r') as file:
+    data = json.load(file)
+    # print(reader)
+    for item in data:
+          mycursor.execute("""
+                          INSERT IGNORE INTO tweets (id, texto, usuario, hashtags, fecha, retweets, favoritos)
+                          VALUES (%s, %s, %s, %s, %s, %s, %s)
+                          """, (item['id'], item['texto'], item['usuario'], json.dumps(item['hashtags']), item['fecha'], item['retweets'], item['favoritos']))
+                                                                            
+          print(mycursor.rowcount, "row got inserted")
+    mydb.commit()
+    
+
+mycursor.close()
+mydb.close()
+
+print("Done")
+
+
+# sql = 'INSERT INTO tweets (id, texto, usuario, hashtags, fecha, retweets, favoritos) VALUES (%s,%s,%s,%s,%s,%s,%s)'
+
+
+
+
+# mycursor.execute('SELECT * FROM tweets;')
+
+# myresult = mycursor.fetchall()
+
+# for x in myresult:
+#   print(x)
+
+# if connection.is_connected():
+#     print('Connected Succesfully')
+# else:
+#     print('Failed to connect')
+
